357121067567f62c8df48d8882d4cbd502617eb6
Mohamed Lemine <mohamedleminlehcene@gmail.com>
2023-06-05 22:29:24 +0100

    first commit

---
+++ AppTestAlgorithm.java
@@ -0,0 +1,8 @@
+import ma.enset.qlearning.sequentielle.Qlearning;
+
+public class AppTestAlgorithm {
+    public static void main(String[] args) {
+        Qlearning qlearning = new Qlearning();
+        qlearning.runQlearning();
+    }
+}
